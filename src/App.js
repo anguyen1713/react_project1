609d376ebfdf08ab9d323d6c6f464bc017beda10
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-3.local>
2025-06-27 19:12:22 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my React Page!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I'm currently learning how to use react.
         </a>
       </header>
     </div>
