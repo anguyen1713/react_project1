1d2c64ef0611c84de3c8791e96df20a7e91eda01
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-3.local>
2025-07-08 11:55:37 -0700

    changes to syntax

--- src/App.js
+++ src/App.js
@@ -46,9 +46,7 @@ function App() {
         >
           IEEE Homepage.
         </a>
-        <p>
-          This is the intro to Bruin Racing, UCLA's racing team.
-        </p>
+        <p>This is the intro to Bruin Racing, UCLA's racing team.</p>
         <a
           className="App-link"
           href="https://www.youtube.com/watch?v=5tcUT_gifZs&pp=ygUMYnJ1aW4gcmFjaW5n"
@@ -59,8 +57,8 @@ function App() {
         </a>        
 
         <h2>IEEE Achievements</h2>
-        <p>
-          The IEEE Bruins have numerous accolades and awards, including:
+        
+        <h2>  The IEEE Bruins have numerous accolades and awards, including: </h2>
           <ul className ='App-bodytext'>
             <li>1st Place in the 2023 IEEE Region 6 Robotics Competition</li>
             <li>2nd Place in the 2022 IEEE Region 6 Robotics Competition</li>
@@ -68,31 +66,31 @@ function App() {
             <li>Outstanding Community Service Award in 2020</li>
           </ul>
 
-        </p>
+        
 
         <img src={IEEE_robot} className="App-logo" alt="robots" />
 
         <h2>
         What are some way's to get involved with IEEE?</h2>
-        <p>
+        
           <ul className ='App-bodytext'>
             <li>Attend weekly meetings every Wednesday at 7 PM in the Engineering Building, Room 123</li>
             <li>Participate in our monthly hackathons and coding challenges</li>
             <li>Join our community service projects and outreach programs</li>
             <li>Contribute to our open-source projects on GitHub</li>
           </ul>
-        </p>
+        
         <img src={IEEE_logo} className="App-logo" alt="IEEE Logo" />
 
         <h2>Opportunities at IEEE?</h2>
-          <p>
+          
           <ul className ='App-bodytext'>
             <li>Internships with leading tech companies</li>
             <li>Networking events with industry professionals</li>
             <li>Workshops and seminars on the latest technologies</li>
             <li>Access to exclusive IEEE resources and publications</li>
           </ul>
-        </p>
+        
 
         <img src={pc} className = 'backy' alt="background" />
 
